70729e964e7a95059d30a81853e482b01216d704
1 <1@DESKTOP-MJ026DI>
2020-04-06 17:36:17 +0800

    1

--- Controllers/measurementController.cs
+++ Controllers/measurementController.cs
@@ -9,7 +9,11 @@ namespace NewHope.Controllers
     public class measurementController : Controller
     {
         // GET: measurement
-        public ActionResult Index()
+        public ActionResult layou()
+        {
+            return View();
+        }
+        public ActionResult measure()
         {
             return View();
         }
